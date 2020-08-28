ae6ea7052b2b768ecec84e221e2730fdfe6a7eaa
herlesupreeth <herlesupreeth@gmail.com>
2020-08-28 14:21:42 +0200

    Revert commit which took contact Via host to create IPSec tunnels

--- src/modules/ims_ipsec_pcscf/cmd.c
+++ src/modules/ims_ipsec_pcscf/cmd.c
@@ -685,7 +685,6 @@ int ipsec_create(struct sip_msg* m, udomain_t* d, int _cflags)
 {
     pcontact_t* pcontact = NULL;
     struct pcontact_info ci;
-    ip_addr_t ue_ipsec_addr;
     int ret = IPSEC_CMD_FAIL;   // FAIL by default
 	tm_cell_t *t = NULL;
 
@@ -799,15 +798,7 @@ int ipsec_create(struct sip_msg* m, udomain_t* d, int _cflags)
             }
         }
 
-        //Convert ipsec address from str to struct ip_addr
-        if(str2ip6buf(&ci.via_host, &ue_ipsec_addr) < 0){
-            LM_ERR("Unable to convert UE IP str to addr6 [%.*s]. Lets try for addr4\n", ci.via_host.len, ci.via_host.s);
-            if(str2ipbuf(&ci.via_host, &ue_ipsec_addr) < 0){
-                LM_ERR("Unable to convert UE IP str to addr4 [%.*s]\n", ci.via_host.len, ci.via_host.s);
-                goto cleanup;
-            }
-        }
-        if(create_ipsec_tunnel(&ue_ipsec_addr, s) != 0){
+        if(create_ipsec_tunnel(&req->rcv.src_ip, s) != 0){
             goto cleanup;
         }
 
@@ -850,15 +841,7 @@ int ipsec_create(struct sip_msg* m, udomain_t* d, int _cflags)
             goto cleanup;
         }
 
-        //Convert ipsec address from str to struct ip_addr
-        if(str2ip6buf(&ci.via_host, &ue_ipsec_addr) < 0){
-            LM_ERR("Unable to convert UE IP str to addr6 [%.*s]. Lets try for addr4\n", ci.via_host.len, ci.via_host.s);
-            if(str2ipbuf(&ci.via_host, &ue_ipsec_addr) < 0){
-                LM_ERR("Unable to convert UE IP str to addr4 [%.*s]\n", ci.via_host.len, ci.via_host.s);
-                goto cleanup;
-            }
-        }
-        if(create_ipsec_tunnel(&ue_ipsec_addr, req_sec_params->data.ipsec) != 0){
+        if(create_ipsec_tunnel(&req->rcv.src_ip, req_sec_params->data.ipsec) != 0){
             goto cleanup;
         }
 
